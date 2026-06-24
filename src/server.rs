2486135aa342815386179473754e2e9cecf2ce1f
lab-kolerr <lab.kolerr@kolerr.com>
2026-06-24 22:26:20 +0700

    Initial commit - src/server.rs

---
+++ src/server.rs
@@ -0,0 +1,12 @@
+use actix_web::{web, App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(|| async { "Hello, world!" }))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
